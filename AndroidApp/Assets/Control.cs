107b6f7bcc261cc31ed73a752a1f625479265d5d
RicardoGarrigaRamos <ricardogarriga10@gmail.com>
2021-11-05 19:27:29 -0400

    Ground Detection

--- AndroidApp/Assets/Control.cs
+++ AndroidApp/Assets/Control.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Control : MonoBehaviour
 {
-    public float speed;
-    public float jumpForce;
-
-    Rigidbody2D body;
+    public float speed = 10;
+    public float jumpForce = 10;
+    private BoxCollider2D boxCollider2d;
+    private Rigidbody2D body;
+    [SerializeField] private LayerMask platformLayerMask;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        boxCollider2d = transform.GetComponent<BoxCollider2D>();
+        body = transform.GetComponent<Rigidbody2D>();
                
     }
 
@@ -23,7 +26,7 @@ public class Control : MonoBehaviour
         float moveX = SimpleInput.GetAxis("Horizontal");
         body.velocity = new Vector2(moveX * speed, body.velocity.y);
 
-        if(Input.GetKey(KeyCode.Space))
+        if(IsGrounded() && Input.GetKeyDown(KeyCode.Space))
         {
             body.velocity = new Vector2(body.velocity.x, jumpForce);
         }        
@@ -32,6 +35,13 @@ public class Control : MonoBehaviour
     public void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, jumpForce);
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d.collider != null;
     }
 }
 
