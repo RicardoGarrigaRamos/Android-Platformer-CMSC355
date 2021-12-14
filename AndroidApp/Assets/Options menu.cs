0e350ad7278b4d5676606db7b63eb2db5c7cd0ec
knntholvr <kenolivar1006@gmail.com>
2021-12-14 00:27:24 -0500

    Create Options menu.cs

---
+++ AndroidApp/Assets/Options menu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Optionsmenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+    public Dropdown qualityDropdown;
+    public Dropdown textureDropdown;
+    public Dropdown aaDropdown;
+    public Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
